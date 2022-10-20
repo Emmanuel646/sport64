9e8227b033486872bbdea60173b9f37b6b4c3800
Emmanuel BROIS <mtdoctor64@gmail.com>
2022-10-20 10:05:39 +0200

    add js confirm

---
+++ public/assets/js/script.js
@@ -0,0 +1,21 @@
+    function activeConfirme()  {
+
+    var result = confirm("Voulez vous activer la structure ?");
+
+    if(result)  {
+    alert("Vous venez d'activer la struture !");
+} else {
+    alert("La structure n'a pas été activer.");
+}
+}
+
+    function desactiveConfirme()  {
+
+    var result = confirm("Voulez vous désactiver la structure ?");
+
+    if(result)  {
+    alert("Vous venez de désactiver la struture !");
+} else {
+    alert("La structure n'a pas été désactiver.");
+}
+}
