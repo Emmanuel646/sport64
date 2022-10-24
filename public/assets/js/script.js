68232c3296cfc791df8ff0059e246528acf63848
Emmanuel BROIS <mtdoctor64@gmail.com>
2022-10-24 10:29:23 +0200

    remove maker-bundle dev

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -19,3 +19,25 @@
     alert("La structure n'a pas été désactiver.");
 }
 }
+
+    function disconnectConfirm()  {
+
+        var result = confirm("Voulez vous, vous déconnectez de la plateforme SPORT 64 ?");
+
+        if(result)  {
+
+        } else {
+            alert("Tout va bien, vous être toujours connecté à la plateforme SPORT 64");
+        }
+    }
+
+    function connectConfirm()  {
+
+        var result = confirm("Vous êtes sur le point de vous connecter à la plateforme SPORT 64 ...");
+
+        if(result)  {
+
+        } else {
+            alert("La connexion à la plateforme a été annulé !");
+        }
+    }
